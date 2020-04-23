a9fc933de9911c03528360785e9b0de792d9598a
dariakharlan <daria@anodot.com>
2020-04-23 15:40:16 +0300

    fix timestamp in tests

--- tests/test_metric.py
+++ tests/test_metric.py
@@ -9,7 +9,7 @@ from datetime import datetime
     ('test', 0, 'gauge', datetime.strptime('2020-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'), {'te. st': 'test. 1'},
      {'ta. g': ['ta. g1', 'tag2']}, 1, {
          'value': 0.0,
-         'timestamp': 1577829600.0,
+         'timestamp': 1577836800.0,
          'properties': {
              'what': 'test',
              'ver': 1,
@@ -20,7 +20,7 @@ from datetime import datetime
      }),
     ('test', '0.01', 'counter', datetime.strptime('2020-01-01 00:00:00', '%Y-%m-%d %H:%M:%S'), None, None, '2', {
         'value': 0.01,
-        'timestamp': 1577829600.0,
+        'timestamp': 1577836800.0,
         'properties': {
             'what': 'test',
             'ver': 2,
